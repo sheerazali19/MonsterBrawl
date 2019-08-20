1f535499d51e8d95793bb3875c3604318688bad9
sheeraz ali <hacker.ascool@gmail.com>
2019-08-20 13:01:02 +0530

    Updates

---
+++ precache-manifest.476fa093e56468151d60f41fde40d2cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b5745d8215cdb218135c409f5a58e0c",
+    "url": "/MonsterBrawl/index.html"
+  },
+  {
+    "revision": "dab35c015f697a491beb",
+    "url": "/MonsterBrawl/static/css/main.0d9c540d.chunk.css"
+  },
+  {
+    "revision": "3dc783ba8e887e1099c1",
+    "url": "/MonsterBrawl/static/js/2.54057abf.chunk.js"
+  },
+  {
+    "revision": "dab35c015f697a491beb",
+    "url": "/MonsterBrawl/static/js/main.9e54e852.chunk.js"
+  },
+  {
+    "revision": "0959b4fbbc26e4b036d2",
+    "url": "/MonsterBrawl/static/js/runtime~main.db37bbd2.js"
+  }
+]);
